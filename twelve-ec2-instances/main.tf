44b41eafc47e5b7c23d6e0804b87b1433b0ee8cb
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-22 19:04:04 +0530

    expense ansible forks dynamic inventory

--- twelve-ec2-instances/main.tf
+++ twelve-ec2-instances/main.tf
@@ -17,3 +17,25 @@ resource "aws_route53_record" "expense_r53" {
     
     allow_overwrite = true
 }
+resource "aws_instance" "devops_ec2" {
+    ami           = data.aws_ami.rhel_info.id
+    instance_type = var.devops_ec2.instance_type
+    vpc_security_group_ids = [var.allow_all]
+    user_data = file("${path.module}/ansible.sh")
+    # user_data = <<-EOF
+    #           #!/bin/bash
+    #           sudo dnf install ansible -y       
+    #           EOF
+    tags = {
+        Name = "devops_ec2"
+    }
+    depends_on = [aws_instance.expense] 
+}
+resource "aws_route53_record" "devops_r53" {
+    zone_id = var.zone_id
+    name    = "devops.${var.domain_name}"
+    type    = "A"
+    ttl     = 1
+    records = [aws_instance.devops_ec2.public_ip]
+    allow_overwrite = true
+}
